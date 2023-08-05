69fef7fa76e05d192861acb8bca7bfe830fe96e7
Surbhi <surbhikumaridav@gmail.com>
2023-08-05 12:25:36 +0530

    verify completed

--- src/app/auth/verifysuccess/[id]/page.tsx
+++ src/app/auth/verifysuccess/[id]/page.tsx
@@ -1,44 +1,65 @@
-"use client"
-import { RootState } from "@/redux/store";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+"use client";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { getData } from "@/domain/auth/api";
-
+import { toast } from "react-hot-toast";
 
 const VerifySuccess = () => {
-
-    useEffect(() => {
+  const [id, setId] = useState("");
+  const [data, setData] = useState<any>({});
+  useEffect(() => {
     const id = window.location.pathname.split("/")[3];
+    setId(id);
+  }, []);
 
+  useEffect(() => {
     if (id) {
-      getData(`/verifysuccess/`,{},id)
-        .then((data) => {
-          console.log(data);
-        }
-        )
+      getData(`/verifysuccess/`, {}, id).then((data) => {
+        console.log(data);
+        console.log(data === undefined);
+        setData(data);
+      });
     }
-      
-    }, []);
+  }, [id]);
+
+  useEffect(() => {
+      if (data.status === "success") {
+        toast.success(data.message);
+      }
+     else if(data.status === "fail") {
+      toast.error(data.message);
+    }
+}, [data]);
 
   return (
     <div className="flex items-center justify-center flex-col space-y-3 text-gray-600 text-lg p-28">
       <div className="text-black font-bold text-4xl">{"Welcome to QUIGO"}</div>
-      <div className="font-semibold">
-        {
-          "Congratulations, sur1611 has been verified!,"
-        }
-      </div>
+      {data !== undefined &&
+      data.message !== "Invalid verification code or user doesn't exists" ? (
+        data.status === "success" ? (
+          <div className="font-semibold">
+            {`Congratulations, ${data.data}  has been verified!,`}
+          </div>
+        ) : (
+          data.message === "User already verified" &&
+          data.status === "fail" &&
+          data.data === undefined && (
+            <div className="font-semibold">{`User already verified`}</div>
+          )
+        )
+      ) : (
+        <div className="font-semibold">
+          {"Invalid verification link or or user doesn't exists"}
+        </div>
+      )}
       <div className="text-sm">
         {`Riding into the future with Quigo: Where every journey is not just a ride, but a secure and seamless experience powered by blockchain technology.`}
       </div>
-      <Link className="bg-blue-500 text-white p-2 rounded" href="/auth/login">Get started with Quigo</Link>
+      <Link className="bg-blue-500 text-white p-2 rounded" href="/auth/login">
+        Get started with Quigo
+      </Link>
     </div>
   );
 };
 
 export default VerifySuccess;
-
-
-
