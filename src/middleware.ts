aa2f3a57d9b033c6d73378dd90998c5ca9bdf7eb
Surbhi <surbhikumaridav@gmail.com>
2023-08-31 01:09:43 +0530

    middleware issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
-import { useRef } from 'react';
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const path = request.url
+  const baseUrl = "https://quigo-fr-9lb3.vercel.app"; 
+  const path = request.nextUrl.pathname;
   console.log(path + "path");
   const isPublicPath =
     path === "/auth/login" ||
@@ -13,29 +13,27 @@ export function middleware(request: NextRequest) {
     path === "/auth/verifysuccess" ||
     path === "/auth/verifysuccess/:path*" ||
     path === "/auth/verifyemail";
-
+    
   const token = request.cookies.get("token")?.value || "";
   const email = request.cookies.get("email")?.value || "";
 
   if (!isPublicPath && !token && !email) {
     if (path !== "/") {
-      return NextResponse.redirect(new URL("/", request.nextUrl));
+      return NextResponse.redirect(new URL("/", baseUrl));
     }
   }
 
   if (isPublicPath && token) {
-    return NextResponse.redirect(new URL("/user/home", request.nextUrl));
+    return NextResponse.redirect(new URL("/user/home", baseUrl));
   }
 
   if (path === "/auth/verifyemail" && !email) {
-    return NextResponse.redirect(new URL("/auth/signup", request.nextUrl));
+    return NextResponse.redirect(new URL("/auth/signup", baseUrl));
   }
 
   if (path.startsWith("/auth/verifysuccess/") && !email) {
-    return NextResponse.redirect(new URL("/auth/signup", request.nextUrl));
+    return NextResponse.redirect(new URL("/auth/signup", baseUrl));
   }
-
-  return Response.redirect(path);
 }
 
 export const config = {
